0da728ad51bf6724e6bcf182f8d7d541b8ff385b
valclassdevelop <developervalclass.com>
2024-05-07 08:41:55 +0700

    mitranet

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,105 @@
 import React, { useState } from "react";
 import { Logo } from "../Aseets";
 import { Link } from "react-router-dom";
-import { FaChevronDown } from "react-icons/fa";
+import { FaChevronDown, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
     const [activeHajiTour, setActiveHajiTour] = useState(false);
     const [activeProduct, setActiveProduct] = useState(false);
+    const [open, setOpen] = useState(false);
+
 
     return (
-        <div className="relative w-screen h-max bg-white hidden lg:flex px-12 py-6 items-center justify-between pz-12 shadow-md z-[9999999999]">
-            <img src={Logo} alt="logo-mitranet" />
+        <div className="relative w-screen h-max bg-white flex px-4 lg:px-12 py-6 items-center justify-between pz-12 shadow-md z-[9999999999]">
+            <img src={Logo} alt="logo-mitranet" className="lg:w-max w-[40%]" />
+            <div className={`absolute bg-white shadow-md z-[444444] ${open ? 'left-0' : 'left-[-100%]'} lg:hidden inline duration-200 top-0 h-screen w-[80%] overflow-x-hidden`}>
+                <ul className="list-none">
+                    <li className="my-12 ml-8 text-grayColor cursor-pointer active:scale-[0.98] hover:text-black duration-100">
+                        Beranda
+                    </li>
+                    <Link to={"/tentang-mitranet"}>
+                        <li className="my-12 ml-8 text-grayColor cursor-pointer active:scale-[0.98] hover:text-black duration-100">
+                            Tentang Mitra
+                        </li>
+                    </Link>
+                    <li className="my-12 z-[33] relative ml-8 text-grayColor cursor-pointer duration-100">
+                        <p
+                            className="hover:text-black flex items-center"
+                            onClick={() => {
+                                setActiveHajiTour(!activeHajiTour),
+                                    setActiveProduct(false);
+                            }}
+                        >
+                            Haji & Wisata{" "}
+                            <FaChevronDown className="ml-2 text-[12px]" />
+                        </p>
+                        <div
+                            className={`w-max h-max px-2 py-3 bg-white rounded-[12px] shadow-lg absolute ${
+                                activeHajiTour
+                                    ? "bottom-[-120px] left-[-20px] [999] opacity-[1]"
+                                    : "z-[-1] opacity-[0] bottom-[-50px] left-[-20px]"
+                            } border border-slate-200 flex flex-col items-center justify-center`}
+                        >
+                            <Link to={"/haji"}>
+                                <div
+                                    className={`px-4 py-2 font-[600] text-center w-full bg-[#568660] text-white h-[40px] rounded-[12px] cursor-pointer hover:brightness-[90%] active:scale-[0.98] duration-200`}
+                                >
+                                    Haji & Umrah
+                                </div>
+                            </Link>
+                            <Link to={"/wisata"}>
+                                <div
+                                    className={`px-4 py-2 font-[600] text-center w-full h-[40px] rounded-[12px] cursor-pointer hover:brightness-[90%] active:scale-[0.98] duration-200`}
+                                >
+                                    Wisata
+                                </div>
+                            </Link>
+                        </div>
+                    </li>
+                    <li className="my-12 relative ml-8 text-grayColor cursor-pointer duration-100">
+                        <p
+                            className="hover:text-black flex items-center"
+                            onClick={() => {
+                                setActiveProduct(!activeProduct),
+                                    setActiveHajiTour(false);
+                            }}
+                        >
+                            Produk{" "}
+                            <FaChevronDown className="ml-2 text-[12px]" />
+                        </p>
+                        <div
+                            className={`w-max h-max pt-3 pb-2 bg-white rounded-[12px] shadow-lg absolute ${
+                                activeProduct
+                                    ? "bottom-[-120px] left-[-20px] [999] opacity-[1]"
+                                    : "z-[-1] opacity-[0] bottom-[-50px] left-[-20px]"
+                            } border border-slate-200 flex flex-col items-center justify-center`}
+                        >
+                            <Link to={"/emas"}>
+                                <div
+                                    className={`px-4 py-2 font-[600] text-center w-full bg-[#568660] text-white h-[40px] rounded-[12px] cursor-pointer hover:brightness-[90%] active:scale-[0.98] duration-200`}
+                                >
+                                    Emas
+                                </div>
+                            </Link>
+                            <Link to={"/dana-tunai"}>
+                                <div
+                                    className={`px-4 py-2 font-[600] text-center w-full h-[40px] rounded-[12px] cursor-pointer hover:brightness-[90%] active:scale-[0.98] duration-200`}
+                                >
+                                    Dana Tunai
+                                </div>
+                            </Link>
+                        </div>
+                    </li>
+                    <li className="my-12 ml-8 text-grayColor cursor-pointer active:scale-[0.98] hover:text-black duration-100">
+                        Hubungi Kami
+                    </li>
+                    <li className="my-12 ml-8 text-grayColor cursor-pointer active:scale-[0.98] hover:text-black duration-100">
+                        FAQ
+                    </li>
+                </ul>
+            </div>
             <div className="w-full flex items-center justify-center">
-                <ul className="list-none flex items-center">
+                <ul className="list-none hidden lg:flex items-center ">
                     <li className="ml-8 text-grayColor cursor-pointer active:scale-[0.98] hover:text-black duration-100">
                         Beranda
                     </li>
@@ -96,9 +184,21 @@ const Navbar = () => {
                     </li>
                 </ul>
             </div>
-            <div className="w-max px-8 h-max py-3 bg-mainColor text-white flex items-center justify-center ml-[100px] rounded-[8px] text-white cursor-pointer hover:brightness-[90%] active:scale-[0.98]">
+            <div className="w-max px-8 h-max py-3 bg-mainColor text-white hidden lg:flex items-center justify-center ml-[100px] rounded-[8px] text-white cursor-pointer hover:brightness-[90%] active:scale-[0.98]">
                 <p>Daftar</p>
             </div>
+            {
+                open ? (
+                    <div onClick={() => setOpen(false)} className="w-[90px] h-[40px] lg:hidden inline bg-red-500 text-white cursor-pointer active:scale-[0.98] flex items-center rounded-[4px] justify-center">
+                        <FaTimes />
+                    </div>
+                ):
+                    <div onClick={() => setOpen(!open)} className="ml-auto  lg:hidden inline cursor-pointer hover:brightness-[90%] active:scale-[0.98] duration-100 lg:hidden flex flex-col items-center justify-between h-[40px] w-[70px] border border-slate-200 rounded-[4px] p-2">
+                        <div className="w-[90%] h-[2px] bg-slate-600"></div>
+                        <div className="w-[90%] h-[2px] bg-slate-600"></div>
+                        <div className="w-[90%] h-[2px] bg-slate-600"></div>
+                    </div>
+            }
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Routers from './Routers'
 import './index.css'
 
+const Loading = () => {
+  return (
+    <div className='fixed w-screen h-screen z-[4444] flex items-center justify-center'>
+        <p className='text-center text-[16px] animate-pulse'>Loading....</p>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <Routes>
-      {
-          Routers.map((router, index) => (
-            <Route  
-              key={index}
-              path={router.path}
-              element={<router.component />}
-            />
-          ))
-        }
-      </Routes>
+      <Suspense fallback={<Loading />}>
+          <Routes>
+            {
+              Routers.map((router, index) => (
+                <Route  
+                  key={index}
+                  path={router.path}
+                  element={<router.component />}
+                />
+              ))
+            }
+          </Routes>
+        </Suspense>
     </Router>
 </React.StrictMode>,
 )
